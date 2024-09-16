748b33181931e7de4cc701741b92ec0709666171
Benzmovies799 <128473684+Benzmovies799@users.noreply.github.com>
2024-09-16 10:39:10 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -48,7 +48,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [AMROBOTS NETWORKS™](https://t.me/AmRobots_Bots)**"
+        cap = "**BY›› [BENZMOVIES™](https://t.me/Benzmovies)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
